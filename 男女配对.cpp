ce316d88d7ebad1af05cafa0890cdf21dff01c42
2859398864 <45835902+2859398864@users.noreply.github.com>
2019-04-19 19:42:17 +0800

    Update 男女配对.cpp

--- 男女配对.cpp
+++ 男女配对.cpp
@@ -1,31 +1,3 @@
-/*
-One day n girls and n boys come to Xi¡¯an to look for a mate. 
-Each girl has a value a[i], each boy has a value b[i]. 
-Girl i and boy j will fall in love only if a[i] + b[j] ¡Ý k.
-Please help them make pairs as many as possible .
-
-First line an integer T (1 ¡Ü T ¡Ü 10). Indicates the number of test cases.
-Then T test cases follows. Each test case begins with 
-two integer N, K (1 ¡Ü N ¡Ü 200000,0 ¡Ü K ¡Ü 109). 
-The next line has N integers 
-indicate a[1] to a[N] (0 ¡Ü a[i] ¡Ü 109). 
-The next line has N integers indicate b[1] to b[N] (0 ¡Ü b[i] ¡Ü 109)
-
-For each test case, print the answer in a single line.
-
-input
-1
-3 4
-1 2 3
-1 2 3
-
-output
-
-3
-
-
-
-*/
 #include <iostream>
 #include<algorithm>
 #include<queue>
